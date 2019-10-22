bbcb25cf16969e098043d256e1efc710627f9f24
Stanislav Sergienya <skilful522@gmail.com>
2019-10-22 12:25:39 +0300

    minor fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Header } from "./components/Header/Header";
 import { ToDoList } from "./components/ToDoList/ToDoList";
 import { NewToDoItem } from "./components/NewToDoItem/NewToDoItem";
-import style from "./App.module.css";
+import './App.css'
 import moment from "moment";
 
 function App() {
   const date = moment().format("YYYY-MM-DD");
   return (
-    <div id={style.mainContainer}>
+    <div id="mainContainer">
       <Header />
       <ToDoList />
       <NewToDoItem date={date} />
